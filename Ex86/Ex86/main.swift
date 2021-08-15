5af2fb09640cab9e3ace0cab7e4d0cfc08eee8c2
Danielly Alves Ribeiro Machado <daniellyalvesribeiro@gmail.com>
2021-08-14 21:44:43 -0300

    Added exercise 86

---
+++ Ex86/Ex86/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Ex86
+//
+//  Created by Lidiomar Fernando dos Santos Machado on 14/08/21.
+//
+
+import Foundation
+
+HelloWorld()
+
+func HelloWorld() {
+
+print("Hello, World!")
+	
+}
