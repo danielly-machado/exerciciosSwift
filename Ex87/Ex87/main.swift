a4d3097080605c77c23dace0ad168054d51da1ad
Danielly Alves Ribeiro Machado <daniellyalvesribeiro@gmail.com>
2021-08-14 21:49:26 -0300

    Added exercise 87

---
+++ Ex87/Ex87/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Ex87
+//
+//  Created by Danielly Alves Ribeiro Machado on 14/08/21.
+//
+
+import Foundation
+
+aprendendoSwift()
+
+func aprendendoSwift() {
+
+print("+-------=======------+")
+print("Aprendendo Swift")
+print("+-------=======------+")
+
+}
