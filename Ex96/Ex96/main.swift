c53cc19b05a10b02daa143042bed274eb92245da
Danielly Alves Ribeiro Machado <daniellyalvesribeiro@gmail.com>
2021-08-16 17:16:30 -0300

    Added exercise 96

---
+++ Ex96/Ex96/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  Ex96
+//
+//  Created by Danielly Alves Ribeiro Machado on 16/08/21.
+//
+
+import Foundation
+
+func principal() {
+	let nota1 = retornaNotaAluno()
+	let nota2 = retornaNotaAluno()
+	let media = mediaNotaAluno(nota1: nota1, nota2: nota2)
+	let mensagem = mostraMediaAluno(mediaFinal: media)
+	print(mensagem)
+}
+
+func mostraMediaAluno(mediaFinal: Float) -> String {
+	return("A média de notas do aluno é: \(mediaFinal)")
+}
+
+func mediaNotaAluno(nota1: Float, nota2: Float) -> Float {
+	let somatorioNotas = nota1 + nota2
+	var mediaAluno = somatorioNotas / 2
+	
+	return mediaAluno
+}
+
+func retornaNotaAluno() -> Float {
+	print("Digite a nota do aluno: ")
+	let notaDigitada = readLine()
+	
+	let notaDigitadaFloat = Float(notaDigitada ?? "0") ?? 0.0
+	
+	return notaDigitadaFloat
+}
+
+principal()
+
+
